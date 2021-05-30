5bd37dce2891d7a6190823ee844aec7c548cfd4d
halhal <hh.25799623@gmail.com>
2021-05-30 18:27:43 +0900

    add automatic adjustment height function for input form

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import * as color from './color'
 import { Button, ConfirmButton } from './Button'
@@ -16,6 +16,14 @@ export function InputForm({
   onCancel?(): void
   className?: string
 }) {
+  const ref = useRef<HTMLTextAreaElement>(null)
+  useEffect(() => {
+    const el = ref.current
+    if (!el) return
+    const { borderTopWidth, borderBottomWidth } = getComputedStyle(el)
+    el.style.height = 'auto'
+    el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`
+  }, [value])
   const disabled = !value?.trim()
   const handleConfirm = () => {
     if (disabled) return
@@ -25,6 +33,7 @@ export function InputForm({
   return (
     <Container className={className}>
       <Input
+        ref={ref}
         autoFocus
         placeholder="Enter a note"
         value={value}
